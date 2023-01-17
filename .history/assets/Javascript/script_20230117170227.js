8facc9818a2b09c4c1ab6c1002c23c0c6e70c7e5
Thuy Nguyen <vanthuy101102@gmail.com>
2023-01-17 17:23:27 +0700

    add new toast

---
+++ .history/assets/Javascript/script_20230117170227.js
@@ -0,0 +1,35 @@
+let Btns = document.querySelectorAll(".template__btn-item");
+// tạo hiệu ứng click vào button
+for(let key in Btns){
+    let btn = Btns[key];
+
+    btn.onclick = (e)=>{
+        btn.classList.toggle("active");
+        e.stopPropagation();
+    }
+};
+
+// bắt sự kiện onscroll
+//scrollTop mặc định = 0
+
+let toTopBtn = document.querySelector(".to__top");
+
+window.onscroll = () =>{
+    
+    if(document.documentElement.scrollTop > 0){
+        toTopBtn.style.display = "block";
+    }
+    else{
+        toTopBtn.style.display = "none";
+    }
+}
+
+//thông báo khi click vào button
+
+let btnSubmit = document.querySelector("#check__btn");
+let toast = document.querySelector("#toastAll");
+let closeToast = document.querySelector("")
+
+btnSubmit.addEventListener("click", ()=>{
+
+})
---
+++ .history/assets/Javascript/script_20230117172018.js
@@ -0,0 +1,44 @@
+let Btns = document.querySelectorAll(".template__btn-item");
+// tạo hiệu ứng click vào button
+for(let key in Btns){
+    let btn = Btns[key];
+
+    btn.onclick = (e)=>{
+        btn.classList.toggle("active");
+        e.stopPropagation();
+    }
+};
+
+// bắt sự kiện onscroll
+//scrollTop mặc định = 0
+
+let toTopBtn = document.querySelector(".to__top");
+
+window.onscroll = () =>{
+    
+    if(document.documentElement.scrollTop > 0){
+        toTopBtn.style.display = "block";
+    }
+    else{
+        toTopBtn.style.display = "none";
+    }
+}
+
+//thông báo khi click vào button
+
+let btnSubmit = document.querySelector("#check__btn");
+let toast = document.querySelector("#toastAll");
+let window
+let closeToast = document.querySelector(".window__toast-close");
+
+btnSubmit.addEventListener("click", ()=>{
+    toast.style.display = "block";
+    setTimeout(()=>{
+        toast.style.animation = "fadeout 0.3s";
+        toast.style.display = "none";
+    },3000);
+})
+
+closeToast.addEventListener("click", ()=>{
+    toast.style.display = "none";
+})
